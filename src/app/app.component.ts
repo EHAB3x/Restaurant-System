cf78a83ace9620ed7bcfe4743bd5666c1925e6c1
EHAB3x <ehabmah6969@gmail.com>
2025-07-16 13:48:19 +0200

    feat: implement sidebar component and remove header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./layouts/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
